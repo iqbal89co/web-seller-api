e652ba18b12130cdb887daee0b30fc24d3be16e9
iqbal89co <iqbalfakhriza089@gmail.com>
2024-12-06 09:34:36 +0700

    fix: perbaikan middleware manage products

--- routes/api.php
+++ routes/api.php
@@ -49,13 +49,19 @@ Route::group(['middleware' => ['api', 'auth:api', 'check.token.expiry', 'check.p
     Route::delete('/{id}', [BrandController::class, 'destroy'])->name('destroy');
 });
 
-Route::group(['middleware' => ['api', 'auth:api', 'check.token.expiry', 'check.permission:manage-products'], 'prefix' => 'products'], function () {
+Route::group(['middleware' => ['api', 'auth:api', 'check.token.expiry'], 'prefix' => 'products'], function () {
     Route::get('/', [ProductController::class, '__invoke'])->name('all');
     Route::get('/export-excel', [ProductController::class, 'export_excel'])->name('export_excel');
     Route::get('/{id}', [ProductController::class, 'detail'])->name('detail');
-    Route::post('/', [ProductController::class, 'insert'])->name('insert');
-    Route::post('/{id}', [ProductController::class, 'update'])->name('update');
-    Route::delete('/{id}', [ProductController::class, 'destroy'])->name('destroy');
+    Route::post('/', [ProductController::class, 'insert'])
+        ->middleware('check.permission:manage-products')
+        ->name('insert');
+    Route::post('/{id}', [ProductController::class, 'update'])
+        ->middleware('check.permission:manage-products')
+        ->name('update');
+    Route::delete('/{id}', [ProductController::class, 'destroy'])
+        ->middleware('check.permission:manage-products')
+        ->name('destroy');
 });
 
 Route::group(['middleware' => ['api', 'auth:api', 'check.token.expiry'], 'prefix' => 'transactions'], function () {
